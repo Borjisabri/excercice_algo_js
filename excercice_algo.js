b2e818ebac2dff0855dbcfacb2a003caae8bf993
borjisabri <sabri.borji@bearstudio.fr>
2023-08-10 17:08:37 +0100

    des excercice algo et js

---
+++ excercice_algo.js
@@ -0,0 +1,168 @@
+// exercice 1
+/** console.log('Bonjour') */
+
+//exercice 2
+/**const prenom="jean";
+console.log('bonjour ',prenom);*/
+
+//excercice 3
+/**const prenom="jean";
+const age = 24;
+console.log("Je m’appelle",prenom,"et j’ai",age,"ans");*/
+
+//excercice 6
+/**const x=16;
+const y=2;
+console.log(x*y);*/
+
+//excercice 8
+/**var a,b,x;
+a=4;
+b=6;
+console.log("avant a =",a,"b =",b);
+x=a;
+a=b;
+b=x;
+console.log("apres a =",a,"b =",b);*/
+
+//excercice 10
+/**
+ * c=13
+ * b=13
+ * a=13
+ */
+
+//excercice 11
+/**
+ * a=3
+ * b=3
+ */
+
+//excerice 12
+/**
+ * a=9+3=12
+ * b=9+12=21
+ * c=9+9=18
+ * b=18+12=30
+ *
+ */
+/*** STRUCTURE CONDITIONNELLE ET STRUCTURE ALTERNATIVE */
+
+//excercice 1
+/**const x=-9;
+if (x==0) {
+    console.log("le nombre est égale 0")
+}
+else{
+    console.log("le nombre est deffirent de 0")
+    if (x>0) {
+        console.log("le nombre est supérieur a 0")
+    }
+    if (x<0) {
+        console.log("le nombre est inférieur a 0")
+    }
+    if (x>=0) {
+        console.log("le nombre est supérieur ou égale 0")
+    }
+    if (x<=0) {
+        console.log("le nombre est inférieur ou égale 0")
+    }
+
+
+
+}*/
+
+//excercice 4
+/**const genre="fille";
+if (genre=="garcon") {
+    console.log('Je suis un garçon')
+}
+else{
+    console.log(`je suis une fille`)
+}*/
+
+//excercice 5
+/**const tem=-5; 
+if (tem<=0) {
+    console.log("Etat solide")
+}
+else if ((tem>=0)&&(tem<=100)) {
+    console.log("Etat liquide")
+}
+else{
+    console.log("Etat gazeux")
+}*/
+
+//excercice 6
+/**const a = 6;
+const b = 17;
+if (a==b) {
+    console.log('Les nombres sont egeaux')
+}
+else{
+    console.log('Les deux nombres sont différents');
+    console.log('la différence est :',Math.abs(a-b));
+}*/
+
+//excercice 8
+/**const jour= "vendredi";
+const heure= 13;
+if (!((jour=='dimanche')||(heure<8)||(heure>19))) {
+    console.log("Les magasins sont ouverts")
+}*/
+
+//excercice 11
+/**const heure = 7;
+const minute = 8;
+const seconde = 9;
+if ((heure>9)||(minute+1>9)||(seconde+1>9)) {
+
+    console.log(heure+"h",minute+1,'et',seconde+1,"secondes")
+}
+else{
+    console.log('0'+heure+':0'+minute+':0'+seconde)
+}*/
+
+//excercice 13
+/**let x, somme;
+const n = 2;
+x = n;
+somme = 0;
+if (n >= 10) {
+  x = x - 10;
+  somme = somme + 10 * 0.1;
+  if (x >= 20) {
+    x = x - 20;
+    somme = somme + 20 * 0.09;
+    if (x != 0) {
+      somme = somme + x * 0.08;
+    }
+  } else {
+    somme = somme + x * 0.09;
+  }
+} else {
+  somme = somme + n * 0.1;
+}
+console.log("la somme est :", somme);*/
+
+//excercice 14
+/**function checkDate(annee,jour,mois) {
+    if (((jour>0)&&(jour<32))&&((mois>0)&&(mois<13))&&(annee>0 && annee.toString().length===4)) {
+        return true
+    }
+    return false
+}
+const annee = 144;
+const mois = 3;
+const jour = 31;
+const isBisextille=(annee%4==0)&&(annee%100!=0)||(annee % 400 == 0);
+if (checkDate(annee,jour,mois)===false) {
+    console.log("La date est incorrecte");
+} else if (((mois==2)&&(isBisextille)&&(jour>29))||((mois==2)&&(!isBisextille)&&(jour>28))) {
+    console.log("La date est incorrecte");
+} else if ((mois==4)||(mois==6)||(mois==9)||(mois==11)&&(jour>30)) {
+    console.log("La date est incorrecte");
+} else {
+    console.log("La date est valide");
+}*/
+
---
+++ excercice_js.js
@@ -0,0 +1,97 @@
+//excercice 1
+/**const array1 = [1, 2, 3, 4 , 5];
+const array2 = [4, 5, 6];
+const newArray=array1.concat(array2);
+console.log('newArray : ',newArray);*/
+
+//excercice 2
+// function chechArray(vari) {
+// //   if (Array.isArray(vari)) {
+// //     return true
+// //   }
+// //   return false
+// Array.isArray(vari)?console.log("c'est un tableau"):console.log("ce n'est pas un tableau",typeof vari)
+// }
+// const tab='ddd';
+// chechArray(tab)
+
+//excercice 3
+// const arrayWithDuplicates = [1, 2, 2, 3, 1, 4, 4, 5];
+// const array3=[6,7];
+// const array=[...new Set(arrayWithDuplicates),array3];
+// console.log(array);
+
+//excercice 4
+// const array1 = [9, 2, 18, 1, 0, 4, 5];
+// const array2 = [4, 5, 6];
+// const resultat = array1.concat(array2);
+// const cleanArray = [...new Set(resultat)];
+// console.log(cleanArray);
+// console.log(cleanArray.sort((a,b)=>b-a));
+
+//practice 1
+// const tweet = "Excited for the #weekend! #FunTimesAhead #SaturdayVibes";
+// const words = tweet.split(' ');
+// console.log(words);
+// const hachtag = words.filter((w)=>w.includes("Fun"))
+// console.log(hachtag);
+
+//practice 4
+//const date="2023-08-10";
+//const [year,month,day]=date.split('-');
+// console.log(year,month,day);
+// console.log(day+'/'+month+'/'+year);
+
+// const datArray=date.split('-');
+// const formatted_Date=datArray.reverse().join('/');
+// console.log(formatted_Date);
+
+//practice 5
+// const numbers = [2, 4, 6, 8, 7];
+// console.log(numbers.some(a => a%5==0));
+
+//practice 6
+// const numbers = [3, 2, 8, 4, 7];
+// console.log(numbers.every(a => a>1));
+
+//PRACTICE 7
+//const bearArray = ["bear", "beard", "bearstudio", "bears"];
+// console.log(bearArray.some(a => a.includes('sabri')));
+// let check=false;
+// bearArray.forEach((a) => {
+//   if (a.includes("sabri")) {
+//     check=true
+//     return;
+//   }
+// });
+// console.log(check);
+
+
+//function count() {}
+// let countDown = 6;
+// const count = () => {
+//   if (countDown >= 0) {
+//     console.log(countDown);
+//     countDown--;
+//   } else {
+//     console.log("terminer");
+//     clearInterval(interval);
+//   }
+// };
+// const interval = setInterval(count, 2000);
+
+//practice 9
+
+const object={
+name: "sabri",
+age:"22",
+address:{
+    city:'cairo',
+    country : 'egypt',    
+}
+}
+console.log(Object.keys(object).length);
+console.log(Object.entries(object));
+
+
+
